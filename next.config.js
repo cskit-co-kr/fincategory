6e3c804e417daeba6ab75679a6420d6ef748b981
Temuulen <temuulencskit@gmail.com>
2023-04-10 09:26:46 +0800

    hh

--- next.config.js
+++ next.config.js
@@ -7,31 +7,35 @@ module.exports = {
     localeDetection: false,
   },
   images: {
-    domains: ['fincategory.com'],
+    domains: ['fincategory.com', 'cdn5.telegram-cdn.org'],
   },
   env: {
-    API_GET_CATEGORY: process.env.API_GET_CATEGORY
+    API_GET_CATEGORY: process.env.API_GET_CATEGORY,
   },
   async redirects() {
     return [
-        {
-            source: '/',
-            destination: '/search',
-            permanent: true,
-        }]
+      {
+        source: '/',
+        destination: '/search',
+        permanent: true,
+      },
+    ];
   },
   async headers() {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: '/api/:path*',
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
-}
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+};
