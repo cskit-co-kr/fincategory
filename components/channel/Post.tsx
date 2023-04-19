173fbf2eae0829d6bb95a4a73f8bf0e65269328d
bilguun <bilguun.davaadorj@gmail.com>
2023-04-19 10:41:22 +0800

    [id] dynamic page-nii unshih hurdiig nemlee

--- components/channel/Post.tsx
+++ components/channel/Post.tsx
@@ -6,15 +6,15 @@ import {
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { enUS } from "../../lang/en-US";
 import { koKR } from "../../lang/ko-KR";
 import LinkPreview from "./LinkPreview";
 import RenderPost from "./RenderPost";
-import Box from "@mui/material/Box";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import Dialog from "@mui/material/Dialog";
+import dynamic from "next/dynamic";
+const Media = dynamic(() => import("./ChannelMedia"), {
+  ssr: false,
+});
 
 const Post = ({ channel, post }: any) => {
   const router = useRouter();
@@ -126,82 +126,6 @@ const Post = ({ channel, post }: any) => {
         </div>
       </div>
     </div>
-  );
-};
-
-const Media = ({ channel, post }: any) => {
-  const [images, setImages] = useState([]);
-  const [videos, setVideos] = useState([]);
-  const getMediaData = { channel: channel, post: post };
-  useEffect(() => {
-    async function fetchMedia() {
-      try {
-        if (post.media.includes("photo") || post.media.includes("video")) {
-          await fetch(`${process.env.NEXT_PUBLIC_CLIENT_API_URL}/api/media`, {
-            method: "POST",
-            headers: { "content-type": "application/json" },
-            body: JSON.stringify(getMediaData),
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              setImages(data.backgroundImageUrls);
-              setVideos(data.srcValues);
-            });
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchMedia();
-  }, []);
-
-  const [selectedImage, setSelectedImage] = useState(undefined);
-  const handleImageClick = (event: any, image: any) => {
-    setSelectedImage(image);
-  };
-
-  return (
-    <>
-      {images?.length !== 0 || videos?.length !== 0 ? (
-        <Box>
-          <ImageList
-            variant="masonry"
-            cols={images?.length === 1 ? 1 : 2}
-            gap={8}
-          >
-            {images?.map((url: any, index: number) => (
-              <ImageListItem key={index}>
-                <img
-                  src={`${url}?w=248&fit=crop&auto=format`}
-                  srcSet={`${url}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                  alt=""
-                  loading="lazy"
-                  className="max-h-[360px] aspect-auto !object-contain cursor-pointer"
-                  onClick={(event) => handleImageClick(event, url)}
-                />
-              </ImageListItem>
-            ))}
-            {videos?.length > 0 &&
-              videos?.map((url: any, index: number) => (
-                <ImageListItem key={index}>
-                  <video src={url}></video>
-                </ImageListItem>
-              ))}
-          </ImageList>
-          <Dialog
-            open={selectedImage !== undefined}
-            onClose={() => setSelectedImage(undefined)}
-          >
-            <img src={selectedImage} alt="" />
-          </Dialog>
-        </Box>
-      ) : (
-        <div></div>
-        // <div className='bg-gray-100 w-full h-44 flex place-content-center items-center rounded-md'>
-        //   <PhotoIcon className='h-14 w-14 text-gray-300' />
-        // </div>
-      )}
-    </>
   );
 };
 
