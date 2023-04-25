8d82263388a2270b267a1d8ab3ce95ff31fc4c81
bilguun <bilguun.davaadorj@gmail.com>
2023-04-25 15:04:34 +0800

    footer component deer responsive zasvar oruullaa

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,62 +1,66 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React from 'react';
-import { enUS } from '../lang/en-US';
-import { koKR } from '../lang/ko-KR';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import { enUS } from "../lang/en-US";
+import { koKR } from "../lang/ko-KR";
 
 function Footer() {
   const router = useRouter();
   const { locale } = router;
-  const t = locale === 'ko' ? koKR : enUS;
+  const t = locale === "ko" ? koKR : enUS;
   return (
-    <footer className='bg-white py-8 bottom-0 w-full mt-10'>
-      <div className='md:grid md:grid-cols-5 py-4 xl:p-0'>
-        <div className='mb-4'>
-          <div className='mb-8 font-raleway text-lg'>
-            <a href='/' className='hover:no-underline hover:text-current'>
-              <span className='font-bold text-primary'>Fin</span>
+    <footer className="bg-white py-8 bottom-0 w-full mt-10">
+      <div className="sm:grid sm:grid-cols-3 md:grid-cols-5 py-4 xl:p-0">
+        <div className="mb-4">
+          <div className="mb-8 font-raleway text-lg">
+            <a href="/" className="hover:no-underline hover:text-current">
+              <span className="font-bold text-primary">Fin</span>
               Category
             </a>
           </div>
-          <h2 className='font-bold text-base'>{t['contact-us']}</h2>
-          <ul className='list-none mt-3 leading-7'>
+          <h2 className="font-bold text-base">{t["contact-us"]}</h2>
+          <ul className="list-none mt-3 leading-7">
             <li>
-              <Link href='/'>{t['customer-support']}</Link>
+              <Link href="/">{t["customer-support"]}</Link>
             </li>
             <li>
-              <Link href='mailto:jopaint@naver.com'>{t['email']}: jopaint@naver.com</Link>
+              <Link href="mailto:jopaint@naver.com">
+                {t["email"]}: jopaint@naver.com
+              </Link>
             </li>
           </ul>
         </div>
-        <div className='mb-4'>
-          <h2 className='font-bold text-base'>{t['our-channels']}</h2>
-          <ul className='list-none mt-3 leading-7'>
+        <div className="mb-4">
+          <h2 className="font-bold text-base">{t["our-channels"]}</h2>
+          <ul className="list-none mt-3 leading-7">
             <li>
-              <Link href='https://t.me/comaps' target='_blank'>
+              <Link href="https://t.me/comaps" target="_blank">
                 코인 상승 얼럿 Coin Pump Alert
               </Link>
             </li>
           </ul>
         </div>
-        <div className='mb-4'>
-          <h2 className='font-bold text-base'>{t['our-bots']}</h2>
-          <ul className='list-none mt-3 leading-7'>
+        <div className="mb-4">
+          <h2 className="font-bold text-base">{t["our-bots"]}</h2>
+          <ul className="list-none mt-3 leading-7">
             <li>
-              <Link href='/'>@HHGYSBot</Link>
+              <Link href="/">@HHGYSBot</Link>
             </li>
             <li>
-              <Link href='/'>@cskitjopaint_bot</Link>
+              <Link href="/">@cskitjopaint_bot</Link>
             </li>
             <li>
-              <Link href='/'>@FinCategoryBot</Link>
+              <Link href="/">@FinCategoryBot</Link>
             </li>
             <li>
-              <Link href='/'>@GGFTSSBot</Link>
+              <Link href="/">@GGFTSSBot</Link>
             </li>
           </ul>
         </div>
       </div>
-      <div className='mt-0 md:mt-10 text-sm'>© 2023 FinCategory. All Rights Reserved.</div>
+      <div className="mt-0 md:mt-10 text-sm">
+        © 2023 FinCategory. All Rights Reserved.
+      </div>
     </footer>
   );
 }
