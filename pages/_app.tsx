953b1029794d9b2f1782e9156a6f99048bd31056
Davaadorj Jargalsakhan <j.davka@gmail.com>
2023-05-12 13:02:58 +0800

    hi stat update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { GoogleAnalytics } from "nextjs-google-analytics";
 import Layout from "../components/layout";
 import { DataProvider } from "../context/context";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const env = process.env.NODE_ENV;
@@ -117,6 +118,23 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </Layout>
       </DataProvider>
+      <Script
+        id='hi-stat'
+        dangerouslySetInnerHTML={{
+          __html: `
+            var _Hasync= _Hasync|| [];
+            _Hasync.push(['Histats.start', '1,4764338,4,0,0,0,00010000']);
+            _Hasync.push(['Histats.fasi', '1']);
+            _Hasync.push(['Histats.track_hits', '']);
+            (function() {
+            var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+            hs.src = ('//s10.histats.com/js15_as.js');
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+            })();
+          `
+        }}
+      />
+      <noscript><a href="/" target="_blank"><img src="//sstatic1.histats.com/0.gif?4764338&101" alt="free stats" /></a></noscript>
     </>
   );
 }
