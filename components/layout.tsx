6695b90cd4b768da9e052a0cf470555c6240c9e4
Temuulen <temuulencskit@gmail.com>
2023-06-29 16:27:18 +0800

    ff

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Footer from './Footer';
 import Header from './Header';
-import { getSession } from 'next-auth/react';
 
 const Layout = ({ children, memberInfo }: any) => {
   return (
@@ -11,7 +10,7 @@ const Layout = ({ children, memberInfo }: any) => {
       </Head>
 
       <div className='wrapper bg-gray-50'>
-        <Header memberInfo={memberInfo} />
+        <Header />
         <main>
           <div className='container'>{children}</div>
         </main>
@@ -19,23 +18,6 @@ const Layout = ({ children, memberInfo }: any) => {
       </div>
     </>
   );
-};
-
-export const getServerSideProps = async (context: any) => {
-  // Get Member Information
-  let memberInfo = '';
-  const session = await getSession(context);
-  if (session?.user) {
-    const responseMember = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_API_URL}/api/member?f=getuser`, {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-    });
-    memberInfo = await responseMember.json();
-  }
-  // Return
-  return {
-    props: { memberInfo },
-  };
 };
 
 export default Layout;
