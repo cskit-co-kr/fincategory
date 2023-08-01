b7693094f16985b8e08bbd609d230fbe5049d0b5
Davaadorj Jargalsakhan <j.davka@gmail.com>
2023-08-01 13:24:35 +0800

    dd

--- components/channel/Post3.tsx
+++ components/channel/Post3.tsx
@@ -34,7 +34,7 @@ const Post3 = ({ channel, post }: any) => {
         <div className='flex flex-col gap-0.5'>
           <div className='text-sm font-bold'>{channel.title}</div>
           <div className='text-xs text-gray-500'>
-            {new Date(toDateTimeformat(post.date, '-')).toLocaleTimeString(locale === 'ko' ? 'ko-KR' : 'en-US', {
+            {new Date(post.date).toLocaleTimeString(locale === 'ko' ? 'ko-KR' : 'en-US', {
               day: 'numeric',
               month: 'long',
               year: 'numeric',
