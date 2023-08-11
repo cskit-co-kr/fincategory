9f0d9028dd733358881e2a14f95a1fd29c59710f
Temuulen <temuulencskit@gmail.com>
2023-08-11 13:23:00 +0800

    Image to img

--- components/board/ListPostRow.tsx
+++ components/board/ListPostRow.tsx
@@ -49,7 +49,7 @@ const ListPostRow = ({ post, boardName, checkedItems, handleCheckboxChange, user
           </span>
         )}
         {post.extra_01 === '1' && (
-          <Image
+          <img
             src={post.extra_02}
             width='56'
             height='56'
