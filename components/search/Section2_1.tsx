f0047dac249e879201f302dac704228187840131
g.ariunaa11 <g.ariunaa11@gmail.com>
2023-08-22 14:00:38 +0800

    sort channels view

--- components/search/Section2_1.tsx
+++ components/search/Section2_1.tsx
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Channel } from '../../typings';
 import Link from 'next/link';
 import ChannelAvatar from '../channel/ChannelAvatar';
 import { Skeleton } from '@mui/material';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 
 const Section2_1 = ({ channelsToday }: any) => {
+  const router = useRouter();
+  const [sortType, setSortType] = useState(1);
+  const [channelsTotal, setChannelsTotal] = useState(channelsToday);
+  const [channels, setChannels] = useState(channelsToday);
+
+  const switchSortType = (type: number) => {
+    if (type === 2) {
+      setChannels(channelsTotal);
+    } else setChannels(channelsToday);
+
+    setSortType(type)
+  }
+  useEffect(() => {
+    const getChannelTotal = async () => {
+
+      let data: any = {
+        query: null,
+        withDesc: false,
+        category: [],
+        country: [],
+        language: [{ value: 'ko', label: 'Korean' }],
+        channel_type: null,
+        channel_age: 0,
+        erp: 0,
+        subscribers_from: null,
+        subscribers_to: null,
+        paginate: { limit: 5, offset: 0 },
+        sort: { field: 'created_at', order: 'desc' },
+      };
+      data['sort'] = { field: 'total', order: 'desc' };
+      const channelsTotal = await axios
+        .post(`${process.env.NEXT_PUBLIC_API_URL}/client/telegram/searchChannel`, data)
+        .then((response) => response.data.channel);
+      setChannelsTotal(channelsTotal)
+    }
+    getChannelTotal();
+  }, [router]);
+
   return (
     <div className='bg-white border border-gray-200 rounded-xl mx-4 md:mx-0'>
-      <div className='font-bold pt-5 pb-1 px-5'>(오늘)조회수 상위</div>
+      <div className='flex flex-row justify-start align-start'>
+        <div className={`font-bold pt-5 pb-1 pl-5 pr-3 cursor-pointer hover:text-primary ${sortType === 1 && 'text-primary'}`} onClick={() => {
+          switchSortType(1)
+        }}>(오늘)조회수 상위</div>
+        <div className='font-bold pt-5 pb-1 '>{"|"}</div>
+        <div className={`font-bold pt-5 pb-1 px-4 cursor-pointer hover:text-primary ${sortType === 2 && 'text-primary'}`} onClick={() => {
+          switchSortType(2)
+        }}>(누적)조회수 상위</div>
+      </div>
 
-      {channelsToday?.map((channel: Channel, index: number) => {
+      {channels?.map((channel: Channel, index: number) => {
         return (
           <Link
             href={`/channel/${channel.username}`}
