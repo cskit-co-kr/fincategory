80fb2f03160a15f3c861e7af6c0f72594dc158d4
temuulencskit <123712175+temuulencskit@users.noreply.github.com>
2023-11-22 17:36:28 +0800

    af

--- lib/ads2.ts
+++ lib/ads2.ts
@@ -5,12 +5,16 @@ async function addAds2(channels: any) {
 
   const getRandomIndex = (max: any) => Math.floor(Math.random() * (max + 1));
 
+  let newArray = channels;
+
   // Remove items from the end of the main array based on the length of the additional array
-  let newArray = channels.slice(0, channels.length - ads.length);
+  if (channels.length > ads.length) {
+    newArray = channels.slice(0, channels.length - ads.length);
+  }
 
   // Insert the additional array into random positions in the new array
   ads.forEach((item: any) => {
-    const randomIndex = getRandomIndex(newArray.length);
+    const randomIndex = getRandomIndex(newArray.length > 45 ? 45 : newArray.length);
     newArray.splice(randomIndex, 0, item);
   });
 
