e23ba0fbedefce5c4df1846014aae34a3ab91572
temuulencskit <123712175+temuulencskit@users.noreply.github.com>
2023-11-27 13:22:45 +0800

    u

--- pages/api/update-category.ts
+++ pages/api/update-category.ts
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/channel/rowUpdateCategory`, {
     id: req.body.channel_id,
-    category: req.body.category_id,
+    category_id: req.body.category_id,
   });
   const result = await response.data;
   res.status(200).json(result);
