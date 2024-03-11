77727840daf9d61fd22dae69198504213fc393ce
luka0906 <161005802+luka0906@users.noreply.github.com>
2024-03-11 14:28:34 +0800

    naver

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
-import 'rsuite/dist/rsuite-no-reset.min.css';
-import '../styles/globals.css';
+import "rsuite/dist/rsuite-no-reset.min.css";
+import "../styles/globals.css";
 
-import axios from 'axios';
-import { hasCookie, setCookie } from 'cookies-next';
-import { SessionProvider } from 'next-auth/react';
-import { DefaultSeo } from 'next-seo';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import { GoogleAnalytics } from 'nextjs-google-analytics';
-import { useEffect, useState } from 'react';
-import Layout from '../components/layout';
-import { DataProvider } from '../context/context';
-import { useRouter } from 'next/router';
-import NextNProgress from 'nextjs-progressbar';
+import axios from "axios";
+import { hasCookie, setCookie } from "cookies-next";
+import { SessionProvider } from "next-auth/react";
+import { DefaultSeo } from "next-seo";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { GoogleAnalytics } from "nextjs-google-analytics";
+import { useEffect, useState } from "react";
+import Layout from "../components/layout";
+import { DataProvider } from "../context/context";
+import { useRouter } from "next/router";
+import NextNProgress from "nextjs-progressbar";
 
 interface CustomAppProps extends AppProps {
   meta: any; // Replace 'any' with the actual type of your 'meta' property
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps, meta }: CustomAppProps) {
   // const [titleTemplate, setTitleTemplate] = useState('');
   // const [description, setDescription] = useState('');
 
-  const parsedMeta = meta.meta[0].meta.replace(/\n/g, '').replace(/'/g, '"');
+  const parsedMeta = meta.meta[0].meta.replace(/\n/g, "").replace(/'/g, '"');
   const additionalMeta = JSON.parse(parsedMeta);
   const title = meta.meta[0].title;
   const titleTemplate = meta.meta[0].titleTemplate;
@@ -37,16 +37,18 @@ function MyApp({ Component, pageProps, meta }: CustomAppProps) {
 
   useEffect(() => {
     const setVisit = async () => {
-      if (env !== 'development') {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/visit`);
+      if (env !== "development") {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/visit`
+        );
         const data = await res.data;
         if (data.code === 200) {
           const today = new Date();
-          setCookie('visit', today.getTime());
+          setCookie("visit", today.getTime());
         }
       }
     };
-    if (!hasCookie('visit')) {
+    if (!hasCookie("visit")) {
       setVisit();
     }
     // const getMeta = async () => {
@@ -64,8 +66,12 @@ function MyApp({ Component, pageProps, meta }: CustomAppProps) {
   return (
     <>
       <Head>
-        <link rel='icon' href='/favicon.ico' />
-        <link rel='apple-touch-icon' href='/logo.png' />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <meta
+          name="naver-site-verification"
+          content="42d7d5e1feda204b892be0e4ef58262cc9de279b"
+        />
       </Head>
       <DefaultSeo
         title={title}
@@ -73,15 +79,19 @@ function MyApp({ Component, pageProps, meta }: CustomAppProps) {
         description={description}
         additionalMetaTags={additionalMeta}
         twitter={{
-          site: 'finca.co.kr',
+          site: "finca.co.kr",
         }}
       />
 
-      {env === 'development' ? '' : <GoogleAnalytics />}
+      {env === "development" ? "" : <GoogleAnalytics />}
       <SessionProvider session={session}>
         <DataProvider>
           <Layout>
-            <NextNProgress color='#3886E2' height={2} options={{ showSpinner: false }} />
+            <NextNProgress
+              color="#3886E2"
+              height={2}
+              options={{ showSpinner: false }}
+            />
             <Component key={router.asPath} {...pageProps} />
           </Layout>
         </DataProvider>
@@ -91,7 +101,9 @@ function MyApp({ Component, pageProps, meta }: CustomAppProps) {
 }
 
 MyApp.getInitialProps = async () => {
-  const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/meta/read`);
+  const res = await axios.get(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/meta/read`
+  );
   const meta = await res.data;
   return { meta };
 };
