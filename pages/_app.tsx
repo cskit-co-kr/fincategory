20abe2ecad7f3242a4f7be66e6755a1b411e2895
Batnasan123 <bataa5458@gmail.com>
2024-12-10 11:31:12 +0800

    www.googletagmanager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import Layout from "../components/layout";
 import { DataProvider } from "../context/context";
 import { useRouter } from "next/router";
 import NextNProgress from "nextjs-progressbar";
+import Script from "next/script";
 import { url } from "inspector";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -86,6 +87,29 @@ function MyApp({ Component, pageProps }: AppProps) {
           site: "finca.co.kr",
         }}
       />
+
+      {/* GTM Head Script */}
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PLX8NXMH');`,
+        }}
+      />
+
+      {/* GTM NoScript */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-PLX8NXMH"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
 
       {env === "development" ? "" : <GoogleAnalytics />}
       <SessionProvider session={session}>
