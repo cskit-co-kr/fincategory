d4d6d0ec00eded0e3b9c7ef0c0f646861a2affe7
woodygnz <157096039+woodygnz@users.noreply.github.com>
2024-12-19 14:09:42 +0800

    z

--- components/channel/ChannelDetailNav.tsx
+++ components/channel/ChannelDetailNav.tsx
@@ -1,43 +1,87 @@
-import { EyeIcon, HandThumbUpIcon, RectangleGroupIcon, UsersIcon, ChatBubbleLeftRightIcon } from '@heroicons/react/24/outline';
-import { useRouter } from 'next/router';
-import { enUS } from '../../lang/en-US';
-import { koKR } from '../../lang/ko-KR';
-import { Skeleton } from '@mui/material';
+import {
+  EyeIcon,
+  HandThumbUpIcon,
+  RectangleGroupIcon,
+  UsersIcon,
+  ChatBubbleLeftRightIcon,
+} from "@heroicons/react/24/outline";
+import { useRouter } from "next/router";
+import { enUS } from "../../lang/en-US";
+import { koKR } from "../../lang/ko-KR";
+import { Skeleton } from "@mui/material";
 
 const ChannelDetailNav = ({ channel }: any) => {
   const router = useRouter();
   const { locale }: any = router;
-  const t = locale === 'ko' ? koKR : enUS;
-  const menuPath = locale === 'ko' ? '/' : '/en/';
-  const liSelected = 'bg-primary text-white border border-primary';
-  const liNormal = 'bg-gray-50 border border-gray-300 hover:bg-primary hover:text-white hover:border-primary';
-  const aNormal = 'py-2 px-3.5 hover:no-underline flex gap-2 justify-center items-center hover:text-white';
+  const t = locale === "ko" ? koKR : enUS;
+  const menuPath = locale === "ko" ? "/" : "/en/";
+  const liSelected = "bg-primary text-white border border-primary";
+  const liNormal =
+    "bg-gray-50 border border-gray-300 hover:bg-primary hover:text-white hover:border-primary";
+  const aNormal =
+    "py-2 px-3.5 hover:no-underline flex gap-2 justify-center items-center hover:text-white";
   return (
     <>
-      <ul className='w-full text-sm grid sm:flex gap-2 bg-[#f2f2f2] rounded-md p-2 mt-4 md:mt-0'>
-        <li className={`rounded-lg ${router.pathname == '/channel/[id]' ? liSelected : liNormal}`}>
-          <a href={`${menuPath}channel/${channel.username}`} className={aNormal}>
-            <RectangleGroupIcon className='h-3 hidden md:inline' />
-            {t['Summary']}
-          </a>
+      <ul className="w-full text-sm grid sm:flex gap-2 bg-[#f2f2f2] rounded-md p-2 mt-4 md:mt-0">
+        <li
+          className={`rounded-lg ${
+            router.pathname == "/channel/[id]" ? liSelected : liNormal
+          }`}
+        >
+          <div
+            onClick={() => router.push(`/channel/${channel.username}`)}
+            className={aNormal}
+          >
+            <RectangleGroupIcon className="h-3 hidden md:inline" />
+            {t["Summary"]}
+          </div>
         </li>
-        <li className={`rounded-lg ${router.pathname == '/channel/[id]/subscribers' ? liSelected : liNormal}`}>
-          <a href={`${menuPath}channel/${channel.username}/subscribers`} className={aNormal}>
-            <UsersIcon className='h-3 hidden md:inline' />
-            {t['Subscribers']}
-          </a>
+        <li
+          className={`rounded-lg ${
+            router.pathname == "/channel/[id]/subscribers"
+              ? liSelected
+              : liNormal
+          }`}
+        >
+          <div
+            onClick={() =>
+              router.push(`/channel/${channel.username}/subscribers`)
+            }
+            className={aNormal}
+          >
+            <UsersIcon className="h-3 hidden md:inline" />
+            {t["Subscribers"]}
+          </div>
         </li>
-        <li className={`rounded-lg ${router.pathname == '/channel/[id]/posts-views' ? liSelected : liNormal}`}>
-          <a href={`${menuPath}channel/${channel.username}/posts-views`} className={aNormal}>
-            <EyeIcon className='h-3 hidden md:inline' />
-            {t['Posts-reach']}
-          </a>
+        <li
+          className={`rounded-lg ${
+            router.pathname == "/channel/[id]/posts-views"
+              ? liSelected
+              : liNormal
+          }`}
+        >
+          <div
+            onClick={() =>
+              router.push(`/channel/${channel.username}/posts-views`)
+            }
+            className={aNormal}
+          >
+            <EyeIcon className="h-3 hidden md:inline" />
+            {t["Posts-reach"]}
+          </div>
         </li>
-        <li className={`rounded-lg ${router.pathname == '/channel/[id]/comments' ? liSelected : liNormal}`}>
-          <a href={`${menuPath}channel/${channel.username}/comments`} className={aNormal}>
-            <ChatBubbleLeftRightIcon className='h-3 hidden md:inline' />
-            {t['Comments']} ({channel.comment})
-          </a>
+        <li
+          className={`rounded-lg ${
+            router.pathname == "/channel/[id]/comments" ? liSelected : liNormal
+          }`}
+        >
+          <div
+            onClick={() => router.push(`/channel/${channel.username}/comments`)}
+            className={aNormal}
+          >
+            <ChatBubbleLeftRightIcon className="h-3 hidden md:inline" />
+            {t["Comments"]} ({channel.comment})
+          </div>
         </li>
       </ul>
     </>
@@ -45,6 +89,15 @@ const ChannelDetailNav = ({ channel }: any) => {
 };
 
 const ChannelDetailNavSkeleton = () => {
-  return <Skeleton variant='rectangular' sx={{ bgcolor: 'grey.100' }} animation='wave' width='100%' height={54} className='h-min-[54px]' />;
+  return (
+    <Skeleton
+      variant="rectangular"
+      sx={{ bgcolor: "grey.100" }}
+      animation="wave"
+      width="100%"
+      height={54}
+      className="h-min-[54px]"
+    />
+  );
 };
 export { ChannelDetailNavSkeleton, ChannelDetailNav };
